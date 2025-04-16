b6a09c8541debeee143e074b0f4e325659517d11
caponne1 <p.caponne@hotmail.com>
2025-04-16 11:53:47 +0000

    nível aventureiro

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-
 int main()
 {
-    int linha, linha2, coluna, coluna2;
+    int linha, linha2, linha3, linha4, coluna, coluna2, coluna3, coluna4;
     int tabuleiro[10][10];
+
 
     for (int i = 0; i < 10; i++)
     {
@@ -14,39 +14,60 @@ int main()
         }
     }
 
-    printf("digite a linha do navio 1: \n");
-    scanf("%d",&linha);
-    printf("digite a coluna do navio 1: \n");
-    scanf("%d",&coluna);
+    
+    printf("Digite a linha do navio na horizontal: \n");
+    scanf("%d", &linha);
+    printf("Digite a coluna central do navio na horizontal: \n");
+    scanf("%d", &coluna);
+
+    for (int i = -1; i <= 1; i++)
+    {
+        tabuleiro[linha - 1][coluna + i] = 3;
+    }
+
+    
+    printf("Digite a linha central do navio na vertical: \n");
+    scanf("%d", &linha2);
+    printf("Digite a coluna do navio na vertical: \n");
+    scanf("%d", &coluna2);
+
+    for (int i = -1; i <= 1; i++)
+    {
+        tabuleiro[linha2 + i][coluna2 - 1] = 3;
+    }
+
+    
+    printf("Digite a linha inicial do navio 1 em diagonal : \n");
+    scanf("%d", &linha3);
+    printf("Digite a coluna inicial do navio 1 em diagonal : \n");
+    scanf("%d", &coluna3);
 
     for (int i = -1; i < 2; i++)
     {
-        tabuleiro[linha - 1][ coluna + i] = 3;
+        tabuleiro[linha3 + i][coluna3 + i] = 3;
     }
 
-    printf("digite a linha do navio 2: \n");
-    scanf("%d",&linha2);
-    printf("digite a coluna do navio 2: \n");
-    scanf("%d",&coluna2);
+    
+    printf("Digite a linha inicial do navio 2 em diagonal : \n");
+    scanf("%d", &linha4);
+    printf("Digite a coluna inicial do navio 2 em diagonal : \n");
+    scanf("%d", &coluna4);
 
     for (int i = -1; i < 2; i++)
     {
-        tabuleiro[linha2 + i][ coluna2 - 1] = 3;
+        tabuleiro[linha4 + i][coluna4 - i] = 3;
     }
 
-
+    // Mostra o tabuleiro
+    printf("\nTabuleiro:\n");
     for (int i = 0; i < 10; i++)
     {
         for (int j = 0; j < 10; j++)
         {
-            printf("%d ",tabuleiro[i][j]);
+            printf("%d ", tabuleiro[i][j]);
         }
         printf("\n");
     }
-    
 
-
-
-
-
+    return 0;
 }
